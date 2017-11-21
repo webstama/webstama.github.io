b86bda77b4705c8328cadb3c39cd164336bec859
webstama <30627838+webstama@users.noreply.github.com>
2017-11-21 09:56:44 -0500

    Add files via upload

---
+++ tribute/tributepage.js
@@ -0,0 +1,17 @@
+ $(function(){
+                $("#HighSchool").click(function(){
+                    $(".ul").slideToggle("slow");
+                        });
+                             })
+         
+             $(function(){
+                $("#College").click(function(){
+                    $(".ol").slideToggle("fast");
+                        });
+                             })
+         
+              $(function(){
+                    $("#NBA").click(function(){
+                        $(".NBA").slideToggle(1000);
+                            });
+                                })
